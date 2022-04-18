b4a5199bcc4a15a5c42794b5b4b190fa4fdb0c66
Bilal Bora <bilal.bora@ogr.sakarya.edu.tr>
2022-04-18 04:38:05 +0300

    Add project files.

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Nationality { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Bilal";
+            customer1.Id = 7;
+            customer1.Nationality = "Turkey";
+            customer1.Surname = "Bora";
+            customer1.Age = 22;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Hilal";
+            customer2.Id = 2;
+            customer2.Nationality = "Turkey";
+            customer2.Surname = "Bora";
+            customer2.Age = 18;
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Halil";
+            customer3.Id = 3;
+            customer3.Nationality = "Turkey";
+            customer3.Surname = "Bora";
+            customer3.Age = 16;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            foreach (var customer in customers)
+            {
+                customerManager.Add(customer);
+                Console.WriteLine("-----------");
+                customerManager.Delete(customer);
+                Console.WriteLine("-----------");
+                customerManager.Uptade(customer);
+                Console.WriteLine("-----------");
+            }
+            
+        }
+    }
+}
